9a0562f3892dbcf556e5d063cc79e5e2f42e9766
Sagar-Lekhwani <sgrlekhwani@gmail.com>
2023-12-25 16:59:56 +0530

    signup theme update

--- my-giphy-app/app/signUp/page.jsx
+++ my-giphy-app/app/signUp/page.jsx
@@ -1,53 +1,70 @@
-'use client'
+'use client';
 import { useState } from 'react';
-import {useCreateUserWithEmailAndPassword} from 'react-firebase-hooks/auth'
-import {auth} from '@/app/firebase/config'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth } from '@/app/firebase/config';
+import { useRouter } from 'next/navigation';
 
-const SignUp = () => {
+const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const router = useRouter();
+
   const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
 
-  const handleSignUp = async () => {
+  const handleSignup = async () => {
     try {
-        const res = await createUserWithEmailAndPassword(email, password)
-        console.log({res})
-        sessionStorage.setItem('user', true)
-        setEmail('');
-        setPassword('')
-
-    } catch(e){
-        console.error(e)
+      const res = await createUserWithEmailAndPassword(email, password);
+      console.log("res => ", res);
+      setEmail('');
+      setPassword('');
+      router.push('/');
+    } catch (e) {
+      console.error(e);
     }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900">
-      <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96">
-        <h1 className="text-white text-2xl mb-5">Sign Up</h1>
-        <input 
-          type="email" 
-          placeholder="Email" 
-          value={email} 
-          onChange={(e) => setEmail(e.target.value)} 
-          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
-        />
-        <input 
-          type="password" 
-          placeholder="Password" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
-          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
-        />
-        <button 
-          onClick={handleSignUp}
-          className="w-full p-3 bg-indigo-600 rounded text-white hover:bg-indigo-500"
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-indigo-500">
+      <div className="bg-white p-8 rounded shadow-md w-96 text-gray-800">
+        <h2 className="text-3xl font-semibold mb-4">Sign Up</h2>
+        <div className="mb-4">
+          <input
+            type="email"
+            placeholder='Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="password"
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
+          />
+        </div>
+        <button
+          type="submit"
+          disabled={email && password ? false : true}
+          onClick={handleSignup}
+          className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
         >
           Sign Up
         </button>
+        <p className="mt-3 text-sm text-gray-600">
+          Already have an account?{' '}
+          <button
+            onClick={() => router.push('/signIn')}
+            className='text-blue-500 underline'
+          >
+            Sign In
+          </button>
+        </p>
       </div>
     </div>
   );
 };
 
-export default SignUp;
+export default Signup;
